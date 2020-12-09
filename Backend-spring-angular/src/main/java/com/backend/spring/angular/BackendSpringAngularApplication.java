5415688d6f54d9eb5e01b4369446e767a9bef117
Jose_Arredondo <jotarredondo@gmail.com>
2020-12-09 15:26:48 -0300

    Creación de proyecto Backend Api Rest , Java 1.8, SpringBoot 2.4

---
+++ Backend-spring-angular/src/main/java/com/backend/spring/angular/BackendSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.backend.spring.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringAngularApplication.class, args);
+	}
+
+}
---
+++ Backend-spring-angular/src/test/java/com/backend/spring/angular/BackendSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.spring.angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
